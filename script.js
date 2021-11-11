d448f0cac640298ca68c848577e16c289aeadba2
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2021-11-11 23:49:24 +0300

    checked todo

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
 let todosList = [];
 let add = document.getElementById('add');
+let ul = document.getElementsByClassName('list');
+let li = document.getElementsByClassName('list-point');
 
 // add todo and push to []
 function createTodo() {
     let input = document.querySelector('input');
     let todo = {
     task : input.value,
-    // id : Date.now(),
-    // filter : 'all',
-    // completed: '',
+    completed: false,
+    id: Date.now(),
     };
     todosList.push(todo);
     print();
-    console.log(todosList)
+    input.value = '';
+    
 };
 add.addEventListener('click', createTodo);
 document.addEventListener('keydown',function(event){
     if (event.key === 'Enter') {
         createTodo();
     }
-})
+});
+// Checked todo
+document.addEventListener('change', function (event) {
+    //  = event.target.parentElement;
+    let task =  event.target.parentElement;
+    const currentId = task;
+    // const currentTask = todoList.find((task) => task.id === currentId);
+    const span = event.target.nextElementSibling;
+    
+    todosList.find(function(todo){
+    if ( todo.task === span.textContent){
+        todo.completed = !todo.completed;
+    };
+    console.log(todo);
+    });
+    console.log(currentId, span.textContent);
+   
+});
+
+    
+// paint todosList
+
 function print(){
     let ul = document.querySelector("ul.list");
 
@@ -29,9 +52,14 @@ function print(){
         const li = document.createElement('li');
         li.classList.add('list-point');
 
+        const checked = document.createElement('input');
+        checked.type = 'checkbox';
+        
+
         const span = document.createElement('span');
         span.insertAdjacentText('afterbegin', `${todo.task}`);
 
+        li.appendChild(checked);
         li.appendChild(span);
         
         ul.appendChild(li)
