5465abccf283b3e0e26584650b111320404536f7
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2021-11-12 18:33:34 +0300

    test find id

--- script.js
+++ script.js
@@ -23,22 +23,41 @@ document.addEventListener('keydown',function(event){
     }
 });
 // Checked todo
-document.addEventListener('change', function (event) {
+// document.addEventListener('change', function (event) {
     //  = event.target.parentElement;
-    let task =  event.target.parentElement;
-    const currentId = task;
+    // let task =  event.target.parentElement;
+    // const currentId = task;
     // const currentTask = todoList.find((task) => task.id === currentId);
-    const span = event.target.nextElementSibling;
+//     const span = event.target.nextElementSibling;
     
-    todosList.find(function(todo){
-        if ( todo.task === span.textContent){
-            todo.completed = !todo.completed;
-        };
-    });
-   
-});
+//     todosList.find(function(todo){
+//         if ( todo.task === span.textContent){
+//             todo.completed = !todo.completed;
+//         };
+//     });
+//    console.log(todosList)
+// });
+// input.addEventListener('change', toggleTask(todo.id));
+// function toggleTask(id){
+//     todosList.find(id);
+//     console.log(id)
+// }
+function findTodo(todo){
+    if(id === todo.id){
+        return todo;
+    }
+
+};
+
+
 // print todo
 document.addEventListener('dblclick', function(event){
+    const id = event.target.getAttribute('id');
+    const findT = todosList.find((todo) => todo.id === id);
+        
+    console.log('id html',id)
+    console.log('item',findT)
+    console.log('todosList',todosList)
     
 })
 
@@ -53,15 +72,18 @@ function print(){
     todosList.forEach(function(todo){
         const li = document.createElement('li');
         li.classList.add('list-point');
-
-        const checked = document.createElement('input');
-        checked.type = 'checkbox';
+        li.setAttribute('id', `${todo.id}`);
+        
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        
+        // input.addEventListener('change', toggleTask(todo.id));
         
 
         const span = document.createElement('span');
         span.insertAdjacentText('afterbegin', `${todo.task}`);
 
-        li.appendChild(checked);
+        li.appendChild(input);
         li.appendChild(span);
         
         ul.appendChild(li)
